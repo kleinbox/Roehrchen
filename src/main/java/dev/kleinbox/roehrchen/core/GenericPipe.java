7145acd2fc6609a013a4603e539f21289f3d7a9b
Sammy <SammyKoch@pm.me>
2024-10-18 18:55:25 +0200

    Fixed pipe state updating

--- src/main/java/dev/kleinbox/roehrchen/core/GenericPipe.java
+++ src/main/java/dev/kleinbox/roehrchen/core/GenericPipe.java
@@ -12,6 +12,7 @@ import net.minecraft.world.level.block.state.properties.EnumProperty;
 import javax.annotation.Nullable;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static dev.kleinbox.roehrchen.Initialization.LOGGER;
 
@@ -33,6 +34,16 @@ public abstract class GenericPipe extends Block {
                 .add(END_2);
     }
 
+    /**
+     * Returns the pipe next pipe of the given block position
+     * where the side determines the direction to go.
+     *
+     * @param level The level to get the BlockStates from.
+     * @param pos Block position of the current pipe _(does not have to be a pipe actually)_.
+     * @param side The direction to look for the next pipe.
+     *
+     * @return Will either return a pipe or null, if anything else has been found.
+     */
     @Nullable
     public BlockPos getNextPipeFrom(Level level, BlockPos pos, Direction side) {
         BlockPos neighborPos = pos.relative(side);
@@ -54,9 +65,8 @@ public abstract class GenericPipe extends Block {
         Direction end_1 = context.getNearestLookingDirection();
         Direction end_2 = end_1.getOpposite();
 
-        // TODO: Every time a for loop is being used to check both ends, actions are being taken before checking
-        //       the second end as well. This needs to be addressed, as otherwise we might end up with two connectors
-        //       facing the same way...
+        // TODO: This following block does not belong in getStateForPlacement
+        //       should be moved to... updateShape I think?
 
         // We need to check the environment for other pipes and bend those towards us, if possible.
         // We are checking for each end if it is free and bend one of them.
@@ -68,16 +78,21 @@ public abstract class GenericPipe extends Block {
             // Found a pipe in this pipes way.
             BlockState neighbor = context.getLevel().getBlockState(neighborPos);
 
-            for (EnumProperty<Direction> neighborsDirectionProperty : Arrays.asList(END_1, END_2)) {
-                Direction neighborsDirection = neighbor.getValue(neighborsDirectionProperty);
+            List<EnumProperty<Direction>> ends = Arrays.asList(END_1, END_2);
+            for (int i = 0; i<=1; i++) {
+                Direction neighborsDirection = neighbor.getValue(ends.get(i));
 
                 // Checking if the found pipe has a free connector to bend towards us.
                 BlockPos neighborsNeighborPos = getNextPipeFrom(level, neighborPos, neighborsDirection);
                 if (neighborsNeighborPos != null)
                     continue;
 
+                // Making sure the other half is not already bent the same way.
+                if (i == 0 && neighbor.getValue(ends.get(1)) == direction.getOpposite())
+                    break;
+
                 // One connector is free and therefore bendable.
-                BlockState newNeighborsState = neighbor.setValue(neighborsDirectionProperty, direction.getOpposite());
+                BlockState newNeighborsState = neighbor.setValue(ends.get(i), direction.getOpposite());
                 level.setBlock(neighborPos, newNeighborsState, Block.UPDATE_CLIENTS);
                 break;
             }
